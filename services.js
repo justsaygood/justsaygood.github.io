c71c5cc793518544e18c41d5d257bf96723eb8ac
Nina <101993630+justsaygood@users.noreply.github.com>
2022-06-08 17:52:35 +0300

    Update services.js

--- services.js
+++ services.js
@@ -15,7 +15,7 @@ new Swiper('.swiper', {
 });
 
 
-let showAllListBtn = document.querySelector('.button-all__brands);
+let showAllListBtn = document.querySelector('.button-all__brands');
  let showListImg = document.querySelector('.icon-more');
 // блок brands
  let brandList = document.querySelector('.brands-list');
