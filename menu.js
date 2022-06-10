09718ae0430204dc6cb8b7446657c1c9685c500f
Nina Gorodnianskaia <nina_gorodok@mail.ru>
2022-06-10 18:39:02 +0300

    commit june10th

--- menu.js
+++ menu.js
@@ -4,8 +4,18 @@ let closeSideMenuButton = sideMenu.querySelector('.button--burger');
 
 openSideMenuButton.addEventListener('click', function () {
     sideMenu.classList.add('side-wrap--show');
+
+    let pagePos = window.scrollY;
+      document.body.classList.add('body--disabled');
+      document.setAttribute('data-scroll', pagePos);
 });
+
 closeSideMenuButton.addEventListener('click', function () {
     sideMenu.classList.remove('side-wrap--show');
+
+    let pos = parseInt(document.body.getAttribute('data-scroll'));
+    document.body.classList.remove('body--disabled');
+    document.setAttribute('data-scroll', '');
+    window.scrollTo(0, pos);
 });
 
